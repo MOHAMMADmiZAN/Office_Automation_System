e5a935c4c392cdfc330fb342a4b3eb271370f19f
bipon68 <bipon68@users.noreply.github.com>
2023-02-08 19:35:00 +0600

    #12 clear garbage code

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,50 +1,13 @@
 import React from 'react';
-import FormInput from "../components/molecules/Form/FormInput";
-import Btn from "../components/molecules/Form/Btn";
-import {SubmitHandler, useForm} from "react-hook-form";
-import {Container} from "@mui/material";
-import {FormInputType} from "../components/molecules/Form/FormInput/Form_Input";
-
-
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-
 
 import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
-import PersonAdd from '@mui/icons-material/PersonAdd';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
-
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
 import IconTooltip from '../../src/components/molecules/IconTooltip/IconTooltip';
 
 
-
-
-
 const Index: React.FC = (): JSX.Element => {
-
-
-    
-
-
-
 
       const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
       const open = Boolean(anchorEl);
@@ -54,67 +17,25 @@ const Index: React.FC = (): JSX.Element => {
       const handleClose = () => {
         setAnchorEl(null);
       };
-
-
-      const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-      }));
     
 
     return (
         <>
-
-{/* <Grid container spacing={2}>
-  <Grid item xs={8}>
-    <Item>xs=4</Item>
-  </Grid>
-  <Grid item xs={4}>
-    <Item>xs=4</Item>
-  </Grid>
-
-</Grid> */}
-            
-
-      <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', textAlign: 'center' }}>
-        {/* <Typography sx={{ minWidth: 100 }}>Notification</Typography> */}
-        <Tooltip title="Notification">
-            <IconButton
-                onClick={handleClick}
-                size="small"
-                sx={{ ml: 2 }}
-                aria-controls={open ? 'notification-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-            >
-                <Avatar sx={{ width: 32, height: 32 }}>N</Avatar>
-            </IconButton>
-          </Tooltip>
-          
-        <IconTooltip />
-
-
-
-        {/* <Tooltip title="Account settings">
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            sx={{ ml: 2 }}
-            aria-controls={open ? 'account-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-          >
-            <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
-          </IconButton>
-        </Tooltip> */}
-
-      </Box>
-      
-
-            
+          <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', textAlign: 'center' }}>
+            <Tooltip title="Notification">
+                  <IconButton
+                      onClick={handleClick}
+                      size="small"
+                      sx={{ ml: 2 }}
+                      aria-controls={open ? 'notification-menu' : undefined}
+                      aria-haspopup="true"
+                      aria-expanded={open ? 'true' : undefined}
+                  >
+                      <Avatar sx={{ width: 32, height: 32 }}>N</Avatar>
+                  </IconButton>
+                </Tooltip>
+              <IconTooltip />
+            </Box>
         </>
     );
 };
