799933f6841543cda5cb1f842e58dcc25de7ee8d
bipon68 <bipon68@users.noreply.github.com>
2023-02-14 15:32:45 +0600

    #29 initial commit

---
+++ packages/client/src/pages/not-found/notfound.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+interface NOTFOUND_PROPS {
+
+}
+
+
+const NotFound: React.FC<NOTFOUND_PROPS> = () => {
+    return (
+        <>
+            <div>
+                <h1> Not Found</h1>
+            </div>
+        </>
+    );
+};
+
+export default NotFound;
