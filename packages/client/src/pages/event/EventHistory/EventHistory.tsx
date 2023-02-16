652499894a8925e53d41253b9b86c9de4ccaaf49
mohammadMizan <takbir.jcd@gmail.com>
2023-02-16 23:21:21 +0600

    #28 some change in event request

--- packages/client/src/pages/event/EventHistory/EventHistory.tsx
+++ packages/client/src/pages/event/EventHistory/EventHistory.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import EventLayout from "../EventLayout/EventLayout";
 import moment from "moment";
+import {IEventPayloadWithId} from "../../../api/Event.api";
+import {inviteUser} from "../../../components/organisms/CustomModal/InviteUsersModal/InviteUsersModal";
 
 interface EVENT_HISTORY_PROPS {
 
 }
 
 const EventHistory: React.FC<EVENT_HISTORY_PROPS> = (props): JSX.Element => {
-    const handleRowFunc = (date:Date) => {
-        return moment(date).isBefore(moment());
-    };
+
+    const handleRowFunc = (item: IEventPayloadWithId) => {
+
+        return moment(item.endTime).isBefore(moment())
+
+
+    }
     return (
         <>
-            <EventLayout  isBodyRowFuncDate={handleRowFunc} label={`event-list`}/>
+            <EventLayout  isBodyRowFunc={handleRowFunc} label={`event-list`}/>
 
 
         </>
