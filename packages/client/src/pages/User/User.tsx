d1dcc4912946bde579f975bc31143cdb70ea6ac1
mohammadMizan <takbir.jcd@gmail.com>
2023-02-17 12:02:04 +0600

    #49 add user add

---
+++ packages/client/src/pages/User/User.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface USER_PROPS {
+
+}
+
+const User: React.FC<USER_PROPS> = (props): JSX.Element => {
+    return (
+        <>
+
+
+
+        </>
+    );
+};
+
+export default User;
