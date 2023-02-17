c98e1d42f00cdb76c576972bcf8e058849556eb7
Mohammad Mizanur Rahman <58907522+MOHAMMADmiZAN@users.noreply.github.com>
2023-02-17 16:54:09 +0600

    Revert "Fix change event status based on time through cronjob"

--- packages/server/utils/helper.ts
+++ packages/server/utils/helper.ts
@@ -1,4 +1,3 @@
-import moment from 'moment';
 import EventService from "../services/EventService"
 
 const eventService = new EventService();
@@ -6,10 +5,4 @@ const eventService = new EventService();
 export const useCronjob = () => {
     console.log('call check event status')
     eventService.checkEventStatus()
-}
-
-
-export const dateTimeFormat = (dateTime: string | undefined) => {
-    if (!dateTime) return '';
-    return moment(dateTime).toISOString()
 }
