ad3a8ef45206fea45ca34432db1177c03d442064
mohammadMizan <takbir.jcd@gmail.com>
2023-03-13 21:50:16 +0600

    dashboard some

--- packages/client/src/pages/home/index.tsx
+++ packages/client/src/pages/home/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Typography} from "@mui/material";
+import useAuth from "../../hooks/useAuth";
 
 
 const Index: React.FC = (): JSX.Element => {
+    const {user} = useAuth();
 
     return (
 
         <>
+            <Typography variant={`h1`} component={`h1`}>Welcome {user.firstName}</Typography>
         </>
     )
 
